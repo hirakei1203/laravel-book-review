c1e1c1ad1862cda87a65a2596287dbcf6738d613
EC2 Default User <ec2-user@ip-172-31-44-66.ap-northeast-1.compute.internal>
2020-02-29 03:43:36 +0000

    first commit

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string
+     */
+     
+    // 表示エラー（https／httoの混在）の解決のため、記載を変更してます
+    // protected $proxies;
+    protected $proxies = '**';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
